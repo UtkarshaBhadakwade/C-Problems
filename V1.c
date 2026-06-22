d83a6e08acd904fdbe73a418c82bf7961d8fb143
Utkarsha Bhadakwade <utkarshabhdakwade99@gmail.com>
2026-06-22 21:54:34 +0530

    Add practice questions

---
+++ V1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int age=18;
+    float height=5.7;
+    char grade='A';
+    double weight=47.55;
+    printf("AGE:%d\n", age);
+    printf("Height:%f\n", height);
+    printf("Grade:%c\n", grade);
+    printf("Weight:%lf\n", weight);
+    return 0;
+}
