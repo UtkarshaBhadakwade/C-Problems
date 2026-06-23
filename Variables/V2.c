9143ac26fa0f02aa23fdaadb7ee7b5f794326682
Utkarsha Bhadakwade <utkarshabhdakwade99@gmail.com>
2026-06-23 19:14:52 +0530

    Add practice questions

---
+++ Variables/V2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter a integer:");
+    scanf("%d",&a);
+    printf("Enter a second integer:");
+    scanf("%d",&b);
+
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    
+    
+
+    
+    return 0;
+
+    
+
+}
